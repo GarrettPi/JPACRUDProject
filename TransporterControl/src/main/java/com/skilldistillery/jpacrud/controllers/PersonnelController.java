bc2840857c91e6d932ad23e9366e52e96e1d23f7
Garrett Pipes <garrett.pipes@gmail.com>
2021-09-17 20:45:18 -0400

    full stack functional

---
+++ TransporterControl/src/main/java/com/skilldistillery/jpacrud/controllers/PersonnelController.java
@@ -0,0 +1,30 @@
+package com.skilldistillery.jpacrud.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.skilldistillery.jpacrud.data.TransporterDAO;
+
+@Controller
+public class PersonnelController {
+
+	@Autowired
+	private TransporterDAO dao;
+	
+	@RequestMapping(path= {"/", "home.do"})
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("personnelList", dao.findAll());
+		mv.setViewName("home");
+		return mv;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
--- TransporterControl/src/main/java/com/skilldistillery/jpacrud/data/TransporterDaoImpl.java
+++ TransporterControl/src/main/java/com/skilldistillery/jpacrud/data/TransporterDaoImpl.java
@@ -2,16 +2,20 @@ package com.skilldistillery.jpacrud.data;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.jpacrud.entities.Personnel;
 
-
 @Service
 @Transactional
 public class TransporterDaoImpl implements TransporterDAO {
+
+	@PersistenceContext
+	private EntityManager em;
 
 	@Override
 	public Personnel findById(int id) {
@@ -21,10 +25,8 @@ public class TransporterDaoImpl implements TransporterDAO {
 	@Override
 	public List<Personnel> findAll() {
 		String jpql = "SELECT p FROM Personnel p";
-		
-		
-		
-		return null;
+
+		return em.createQuery(jpql, Personnel.class).getResultList();
 	}
 
 }
